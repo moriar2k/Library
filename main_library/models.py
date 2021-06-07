704325e8dbd5f1f241efb6bc86f3fe784e90106c
Antoni Jurczyński <a.jurczynski1@gmail.com>
2021-06-07 22:28:39 +0200

    Dodany model wypożyczeń

--- main_library/models.py
+++ main_library/models.py
@@ -11,3 +11,11 @@ class Bookshelf(models.Model):
     epoch = models.TextField(default='')
     genre = models.TextField(default='')
     quantity = models.IntegerField(default=random.randint(0, 10))
+
+class Rental_list(models.Model):
+    book_id = models.IntegerField(default=None)
+    user_id = models.IntegerField(default=None)
+    check_out_date = models.DateTimeField(auto_now_add=True)
+    planned_date_of_return = models.DateTimeField(default=None)
+    date_of_return = models.DateTimeField(default=None)
+    status = models.TextField(default='Active')
